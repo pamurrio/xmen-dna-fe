a66a0c50a3487e94470095fde97566706a231076
pamurrio <32113484+pamurrio@users.noreply.github.com>
2020-09-18 13:31:17 -0300

    se usa la url del servidor be

--- src/app/dna-xmen.service.ts
+++ src/app/dna-xmen.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-const baseUrl = 'http://localhost:8080/';
+const baseUrl = 'https://xmen-dna-be.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
